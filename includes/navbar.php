47def0abccef75ac3fd923b9dc984080419f2430
Luong Ha Vinh <luonghavinh3011@gmail.com>
2021-10-10 19:52:52 +0700

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -61,7 +61,7 @@
   </a>
   <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <a class="collapse-item" href="listproducts.php">Danh Sách Đơn Hàng</a>
+      <a class="collapse-item" href="listorder.php">Danh Sách Đơn Hàng</a>
     </div>
   </div>
 </li>
