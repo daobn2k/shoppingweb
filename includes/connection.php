1bbfa881c1955b5afc172279032c96da27b15d99
unknown <daovv@sskpi.com>
2021-10-19 20:52:24 +0700

    fix connection

--- includes/connection.php
+++ includes/connection.php
@@ -4,7 +4,7 @@ ob_start();
 $servername = "127.0.0.1";
 $username = "root";
 $password = "";
-$dbname = "shopping_";
+$dbname = "shopping_cart";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
